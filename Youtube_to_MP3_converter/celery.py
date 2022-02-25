f1234f506238e729bed221c075f06e987d6e9e73
Luca Yan <lucayan0506>
2022-02-25 19:09:39 +0000

    3rd commit

--- Youtube_to_MP3_converter/celery.py
+++ Youtube_to_MP3_converter/celery.py
@@ -9,7 +9,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Youtube_to_MP3_converter.settin
 
 
 ## Get the base REDIS URL, default to redis' default
-BASE_REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
 
 app = Celery('Youtube_to_MP3_converterheroku logs --tail')
 
@@ -21,8 +20,3 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
-
-app.conf.broker_url = BASE_REDIS_URL
-
-# this allows you to schedule items in the Django admin.
-app.conf.beat_scheduler = 'django_celery_beat.schedulers.DatabaseScheduler'
